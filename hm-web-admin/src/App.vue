1bffd05eed3cdd1950b97110dde33e130087c8f1
lbh <1074775789@qq.com>
2023-07-29 20:34:30 +0800

    element-plus按需导入

--- hm-web-admin/src/App.vue
+++ hm-web-admin/src/App.vue
@@ -1,6 +1,12 @@
-<script setup></script>
+<script setup>
+const goList = () => {
+  this.$router.push('/list')
+}
+</script>
 <template>
   <div>wsdiv</div>
+  <el-button @click="$router.push('/home')">跳首页</el-button>
+  <button @click="goList">跳列表</button>
 </template>
 
 <style scoped></style>
