52aad40160fbec5d13c242a36e445a4d49a2dd18
lbh <1074775789@qq.com>
2023-08-01 21:52:18 +0800

    代码检查工具

--- hm-web-admin/.eslintrc.cjs
+++ hm-web-admin/.eslintrc.cjs
@@ -36,5 +36,10 @@ module.exports = {
     'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验 (props解构丢失响应式)
     // 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
     'no-undef': 'error'
+  },
+  globals: {
+    ElMessage: 'readonly',
+    ElMessageBox: 'readonly',
+    ElLoading: 'readonly'
   }
 }
