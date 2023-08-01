a2abd90467afb0223a845b737a782696d38acab5
lbh <1074775789@qq.com>
2023-08-01 23:28:29 +0800

    layout架子和登录拦截

--- hm-web-admin/src/router/index.js
+++ hm-web-admin/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +36,15 @@ const router = createRouter({
       ]
     }
   ]
+})
+//登录访问拦截
+//1 undefined / true 是直接放行
+//false拦截from的地址页面
+//3具体路径或路径对象 拦截到对应的地址
+router.beforeEach((to) => {
+  //如果没有token且不是登录页就拦截到登录页
+  const userStore = useUserStore()
+  if (!userStore.token && to.path !== '/login') return '/login'
 })
 
 export default router
--- hm-web-admin/src/views/layout/LayoutContainer.vue
+++ hm-web-admin/src/views/layout/LayoutContainer.vue
@@ -1,6 +1,128 @@
+<script setup>
+import {
+  Management,
+  Promotion,
+  UserFilled,
+  User,
+  Crop,
+  EditPen,
+  SwitchButton,
+  CaretBottom
+} from '@element-plus/icons-vue'
+import avatar from '@/assets/default.png'
+</script>
+
 <template>
-  <div>
-    布局路由
-    <router-view></router-view>
-  </div>
+  <el-container class="layout-container">
+    <el-aside width="200px">
+      <div class="el-aside__logo"></div>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#232323"
+        :default-active="$route.path"
+        text-color="#fff"
+        router
+      >
+        <el-menu-item index="/article/channel">
+          <el-icon><Management /></el-icon>
+          <span>文章分类</span>
+        </el-menu-item>
+        <el-menu-item index="/article/manage">
+          <el-icon><Promotion /></el-icon>
+          <span>文章管理</span>
+        </el-menu-item>
+        <el-sub-menu index="/user">
+          <template #title>
+            <el-icon><UserFilled /></el-icon>
+            <span>个人中心</span>
+          </template>
+          <el-menu-item index="/user/profile">
+            <el-icon><User /></el-icon>
+            <span>基本资料</span>
+          </el-menu-item>
+          <el-menu-item index="/user/avatar">
+            <el-icon><Crop /></el-icon>
+            <span>更换头像</span>
+          </el-menu-item>
+          <el-menu-item index="/user/password">
+            <el-icon><EditPen /></el-icon>
+            <span>重置密码</span>
+          </el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <div>用户名：<strong>李碧豪</strong></div>
+        <el-dropdown placement="bottom-end">
+          <span class="el-dropdown__box">
+            <el-avatar :src="avatar" />
+            <el-icon><CaretBottom /></el-icon>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item command="profile" :icon="User"
+                >基本资料</el-dropdown-item
+              >
+              <el-dropdown-item command="avatar" :icon="Crop"
+                >更换头像</el-dropdown-item
+              >
+              <el-dropdown-item command="password" :icon="EditPen"
+                >重置密码</el-dropdown-item
+              >
+              <el-dropdown-item command="logout" :icon="SwitchButton"
+                >退出登录</el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>©2023 Created by 爱吃香蕉的阿豪</el-footer>
+    </el-container>
+  </el-container>
 </template>
+
+<style lang="scss" scoped>
+.layout-container {
+  height: 100vh;
+  .el-aside {
+    background-color: #232323;
+    &__logo {
+      height: 120px;
+      background: url('@/assets/logo.png') no-repeat center / 120px auto;
+    }
+    .el-menu {
+      border-right: none;
+    }
+  }
+  .el-header {
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .el-dropdown__box {
+      display: flex;
+      align-items: center;
+      .el-icon {
+        color: #999;
+        margin-left: 10px;
+      }
+
+      &:active,
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+  .el-footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    color: #666;
+  }
+}
+</style>
