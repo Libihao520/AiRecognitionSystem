6935b8abc6d9e455a2c19119cae485ed32ecfa93
lbh <1074775789@qq.com>
2023-08-08 22:36:21 +0800

    feat：文章分类架子

--- hm-web-admin/src/views/article/ArticleChannel.vue
+++ hm-web-admin/src/views/article/ArticleChannel.vue
@@ -1,1 +1,7 @@
-<template>频道管理</template>
+<template>
+  <page-container title="文章分类">
+    <template #extra><el-button>添加分类</el-button></template
+    >主体部分，表格
+  </page-container>
+</template>
+<style lang="scss" scoped></style>
