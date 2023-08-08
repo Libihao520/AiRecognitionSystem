2a186b1416692d81948e740d3040cb76ea247733
lbh <1074775789@qq.com>
2023-08-08 23:16:07 +0800

    fix：获取数据错误

--- hm-web-admin/src/views/article/ArticleChannel.vue
+++ hm-web-admin/src/views/article/ArticleChannel.vue
@@ -1,7 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import { artGetChannelsService } from '../../api/article'
+const channelList = ref([])
+const getChannelList = async () => {
+  const res = await artGetChannelsService()
+  channelList.value = res.data.data
+  console.log(channelList.value)
+}
+getChannelList()
+</script>
 <template>
   <page-container title="文章分类">
     <template #extra><el-button>添加分类</el-button></template
-    >主体部分，表格
+    >主体部分，表格+分页
   </page-container>
 </template>
 <style lang="scss" scoped></style>
--- hm-web-admin/src/views/article/ArticleManage.vue
+++ hm-web-admin/src/views/article/ArticleManage.vue
@@ -1,14 +1,3 @@
-<script setup>
-import { ref } from 'vue'
-import { artGetChannelsService } from '../../api/article'
-const channelList = ref([])
-const getChannelList = async () => {
-  const res = await artGetChannelsService()
-  channelList.value = res.data.data
-  console.log(channelList.value)
-}
-getChannelList()
-</script>
 <template>
   <page-container title="文章管理">
     <template #extra><el-button>添加文章</el-button></template
