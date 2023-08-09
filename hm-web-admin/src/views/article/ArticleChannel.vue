a3eb4c1dc9c8f8d8d7f19c5e171efac1f99134ad
lbh <1074775789@qq.com>
2023-08-09 23:08:17 +0800

    feat：渲染表格，添加按钮

--- hm-web-admin/src/views/article/ArticleChannel.vue
+++ hm-web-admin/src/views/article/ArticleChannel.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
 import { artGetChannelsService } from '../../api/article'
 const channelList = ref([])
 const getChannelList = async () => {
@@ -8,11 +9,39 @@ const getChannelList = async () => {
   console.log(channelList.value)
 }
 getChannelList()
+const onEditChannel = (row, $index) => {
+  console.log(row, $index)
+}
 </script>
 <template>
   <page-container title="文章分类">
-    <template #extra><el-button>添加分类</el-button></template
-    >主体部分，表格+分页
+    <template #extra>
+      <el-button>添加分类</el-button>
+    </template>
+    <el-table :data="channelList" style="width: 100%">
+      <el-table-column type="index" label="序号" width="100%"></el-table-column>
+      <el-table-column prop="cate_name" label="分类名称"></el-table-column>
+      <el-table-column prop="cate_alias" label="分类别名"></el-table-column>
+      <el-table-column label="操作">
+        <!-- row 就是channelList的一项，$index下标 -->
+        <template #default="{ row, $index }">
+          <el-button
+            :icon="Edit"
+            circle
+            type="primary"
+            plain
+            @click="onEditChannel(row, $index)"
+          ></el-button>
+          <el-button
+            :icon="Delete"
+            circle
+            type="danger"
+            plain
+            @click="onEditChannel(row, $index)"
+          ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </page-container>
 </template>
 <style lang="scss" scoped></style>
