08e8fe9fde68e24f46b907a8421b03aee6e404d0
Libihao <1074775789@qq.com>
2023-08-10 16:45:42 +0800

    fix：前端去除传参

--- lbh.admin/src/http/index.ts
+++ lbh.admin/src/http/index.ts
@@ -7,8 +7,12 @@ const http = "http://localhost:5157/api"
 export const getToken = (name: string, password: string) => {
     return instance.get(http + "/Login/GetToken?name=" + name + "&Password=" + password);
 }
-export const getfzzjdesktop = (data:{}) => {
+export const getfzzjdesktop = () => {
     //当后端的controller加了权限验证要加上这句，请求带上token
     instance.defaults.headers.common['Authorization']="Bearer "+localStorage["token"];
-    return instance.get(http + "/FzZj/desktop",data);
+    return instance.get(http + "/FzZj/GetDesktop");
+}
+export const getfzzjtable = () => {
+    instance.defaults.headers.common['Authorization']="Bearer "+localStorage["token"];
+    return instance.get(http + "/FzZj/GetTable");
 }
