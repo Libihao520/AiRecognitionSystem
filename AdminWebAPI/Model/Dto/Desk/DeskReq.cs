8334866893afb1ee56cef5fa1d1784ac7a536397
Libihao <1074775789@qq.com>
2023-08-10 10:41:34 +0800

    后端字段补充

--- AdminWebAPI/Model/Dto/Desk/DeskReq.cs
+++ AdminWebAPI/Model/Dto/Desk/DeskReq.cs
@@ -2,5 +2,13 @@ namespace Model.Dto.Desk;
 
 public class DeskReq
 {
+    public string yf { get; set; }
+    public int fz { get; set; }
+    public int sf { get; set; }
+    public int df { get; set; }
     public int hj { get; set; }
+    public int cr { get; set; }
+    public int sy { get; set; }
+    public int jldc { get; set; }
+    public int ck { get; set; }
 }
