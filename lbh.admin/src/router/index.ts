f85ed5638770b2e936bb6f01f8389cb9e0fa749d
lbh <1074775789@qq.com>
2023-08-15 23:11:01 +0800

    feat：路由守卫

--- lbh.admin/src/router/index.ts
+++ lbh.admin/src/router/index.ts
@@ -4,6 +4,8 @@ import DeskTop from '../view/index/DeskTop.vue'
 import RootPage from '../view/index/RootPage.vue'
 import MenuManager from '../view/admin/menu/MenuManager.vue'
 import MenuManager2 from '../view/admin/menu/MenuManager2.vue'
+import Tool from '../global'
+import { UserInfo } from '../../src/view/index/class/UserInfo'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -25,5 +27,30 @@ const router = createRouter({
                 ]
         }
     ]
+})
+const toolObj = new Tool()
+router.beforeEach((to, form) => {
+    if (localStorage["nickname"] != undefined) {
+        const user: UserInfo = JSON.parse(new Tool().FormatToken(localStorage["token"]))
+        const expDate = toolObj.FormatDate(user.exp)
+        const currDate = toolObj.GetDate()
+        if (to.path == "/login") {
+            if (expDate >= currDate) {
+                return { path: "/desktop" }
+            } else {
+                toolObj.ClearLocalStorage()
+            }
+        } else {
+            if (expDate < currDate) {
+                toolObj.ClearLocalStorage()
+                return { path: "/login" }
+            }
+        }
+    } else {
+        //避免无限重定向，因此要做个判断
+        if (to.path !== "/login") {
+            return { path: "/login" }
+        }
+    }
 })
 export default router;
