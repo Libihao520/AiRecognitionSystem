408ece03cf8eccedd573984807bb9ddbc6cc9428
lbh <1074775789@qq.com>
2023-08-19 20:54:39 +0800

    feat：编辑功能

--- hm-web-admin/src/api/article.js
+++ hm-web-admin/src/api/article.js
@@ -16,3 +16,8 @@ export const artGetListService = (params) =>
   request.get('/my/article/list', { params })
 
 export const artPublishService = (data) => request.post('/my/article/add', data)
+//获取文章详情
+export const artGetDetailService = (id) =>
+  request.get('/my/article/info', { params: { id } })
+//编辑文章
+export const artEditService = (data) => request.put('/my/article/info', data)
--- hm-web-admin/src/views/article/components/ArticleEdit.vue
+++ hm-web-admin/src/views/article/components/ArticleEdit.vue
@@ -4,7 +4,13 @@ import ChannelSelect from './ChannelSelect.vue'
 import { Plus } from '@element-plus/icons-vue'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import { artPublishService } from '@/api/article'
+import {
+  artPublishService,
+  artGetDetailService,
+  artEditService
+} from '@/api/article'
+import { baseURL } from '@/utils/request'
+import axios from 'axios'
 
 // 提交
 const emit = defineEmits(['success'])
@@ -17,6 +23,10 @@ const onPublish = async (state) => {
   if (formModel.value.id) {
     // 编辑
     console.log('编辑操作')
+    await artEditService(fd)
+    ElMessage.success('修改成功')
+    visibleDrawer.value = false
+    emit('success', 'edit')
   } else {
     await artPublishService(fd)
     ElMessage.success('添加成功')
@@ -46,12 +56,18 @@ const onSelectFile = (uploadFile) => {
   // 立刻将图片对象，存入 formModel.value.cover_img 将来用于提交
   formModel.value.cover_img = uploadFile.raw
 }
+
 const editorRef = ref()
-const open = (row) => {
+const open = async (row) => {
   visibleDrawer.value = true
-  console.log(row)
   if (row.id) {
-    console.log('回显')
+    // 回显
+    const res = await artGetDetailService(row.id)
+    formModel.value = res.data.data
+    //图片需要单独处理
+    imgUrl.value = baseURL + formModel.value.cover_img
+    const file = await imageUrlToFile(imgUrl.value, formModel.value.cover_img)
+    formModel.value.cover_img = file
   } else {
     console.log('添加')
     //重置from数据
@@ -62,6 +78,28 @@ const open = (row) => {
     editorRef.value.setHTML('')
   }
 }
+// 将网络图片地址转换为File对象
+async function imageUrlToFile(url, fileName) {
+  try {
+    // 第一步：使用axios获取网络图片数据
+    const response = await axios.get(url, { responseType: 'arraybuffer' })
+    const imageData = response.data
+
+    // 第二步：将图片数据转换为Blob对象
+    const blob = new Blob([imageData], {
+      type: response.headers['content-type']
+    })
+
+    // 第三步：创建一个新的File对象
+    const file = new File([blob], fileName, { type: blob.type })
+
+    return file
+  } catch (error) {
+    console.error('将图片转换为File对象时发生错误:', error)
+    throw error
+  }
+}
+
 defineExpose({ open })
 </script>
 
