c9fdfb2d26e443e1506b1ff1aec5832bfc41b6d5
lbh <1074775789@qq.com>
2023-08-19 10:57:39 +0800

    feat：文章管理表单渲染

--- hm-web-admin/src/views/article/ArticleManage.vue
+++ hm-web-admin/src/views/article/ArticleManage.vue
@@ -1,7 +1,83 @@
+<script setup>
+import { Delete, Edit } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const articleList = ref([
+  {
+    id: 5961,
+    title: '新的文章啊',
+    pub_date: '2022-07-10 14:53:52.604',
+    state: '已发布',
+    cate_name: '体育'
+  },
+  {
+    id: 5962,
+    title: '新的文章啊',
+    pub_date: '2022-07-10 14:54:30.904',
+    state: null,
+    cate_name: '体育'
+  }
+])
+
+//编辑
+const onEditArticle = (row) => {
+  console.log(row)
+}
+//删除
+const onDeleteArticle = (row) => {
+  console.log(row)
+}
+</script>
 <template>
   <page-container title="文章管理">
-    <template #extra><el-button>添加文章</el-button></template
-    >主体部分，表格+分页
+    <template #extra><el-button>添加文章</el-button></template>
+    <el-form inline>
+      <el-form-item label="文章分类："
+        ><el-select>
+          <el-option label="新闻" value="110"></el-option>
+          <el-option label="体育" value="137"></el-option> </el-select
+      ></el-form-item>
+      <el-form-item label="发布状态：">
+        <el-select>
+          <el-option label="已发布" value="已发布"></el-option>
+          <el-option label="草稿" value="草稿"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button typr="primary">搜索</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 表格区域 -->
+    <el-table :data="articleList">
+      <el-table-column label="文章标题" prop="title">
+        <template #default="{ row }">
+          <el-link type="primary" :underline="false">{{
+            row.title
+          }}</el-link></template
+        >
+      </el-table-column>
+      <el-table-column label="分类" prop="cate_name"></el-table-column>
+      <el-table-column label="发表时间" prop="pub_date"></el-table-column>
+      <el-table-column label="状态" prop="state"></el-table-column>
+      <el-table-column label="操作">
+        <template #default="{ row }">
+          <el-button
+            circle
+            plain
+            type="primary"
+            :icon="Edit"
+            @click="onEditArticle(row)"
+          ></el-button>
+          <el-button
+            circle
+            plain
+            type="danger"
+            :icon="Delete"
+            @click="onDeleteArticle(row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </page-container>
 </template>
 <style lang="scss" scoped></style>
