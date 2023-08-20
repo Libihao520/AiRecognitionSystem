0c58369ecfc30673dea384499614196bcec99230
lbh <1074775789@qq.com>
2023-08-20 18:37:56 +0800

    feat：选择图片与回显

--- hm-web-admin/src/views/user/UserAvatar.vue
+++ hm-web-admin/src/views/user/UserAvatar.vue
@@ -3,7 +3,16 @@ import { ref } from 'vue'
 import { Plus, Upload } from '@element-plus/icons-vue'
 import { useUserStore } from '@/stores'
 const UserStore = useUserStore()
+const uploadRef = ref()
 const imgUrl = ref(UserStore.user.user_pic)
+const onSelectFile = (uploadFile) => {
+  // 基于 FileReader 读取图片做预览
+  const reader = new FileReader()
+  reader.readAsDataURL(uploadFile.raw)
+  reader.onload = () => {
+    imgUrl.value = reader.result
+  }
+}
 </script>
 <template>
   <page-container title="更换头像">
@@ -17,7 +26,16 @@ const imgUrl = ref(UserStore.user.user_pic)
       <img v-if="imgUrl" :src="imgUrl" class="avatar" />
       <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
     </el-upload>
-    <el-button type="primary" :icon="Plus" size="large">选择图片</el-button>
+
+    <br />
+
+    <el-button
+      @click="uploadRef.$el.querySelector('input').click()"
+      type="primary"
+      :icon="Plus"
+      size="large"
+      >选择图片</el-button
+    >
     <el-button type="success" :icon="Upload" size="large">选择图片</el-button>
   </page-container>
 </template>
